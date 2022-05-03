4b9839a39044d310cd91d5f9f5869a36379232c0
JuanMitidieri <juanmitidieri@gmail.com>
2022-05-02 21:22:21 -0300

    Trigger rebuild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutmeComponent
+    
+    
   ],
   imports: [
     BrowserModule,
